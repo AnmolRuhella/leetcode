f6676cd858deddbf79e344abd3633b9c67eca153
AnmolRuhella <anmolruhella23@gmail.com>
2026-03-13 01:37:50 +0530

    single Number

---
+++ singleNumber.js
@@ -0,0 +1,15 @@
+//Leetcode question number - 136
+
+var singleNumber = function(nums) {
+    const seen = new Set();
+
+    for(const num of nums){
+        if(seen.has(num)){
+            seen.delete(num)
+        }
+        else{
+            seen.add(num)
+        }
+    }
+    return [...seen][0]
+};
