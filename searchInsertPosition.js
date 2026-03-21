95088733b65f936c44a71280d6d18e0a9ac415bf
AnmolRuhella <anmolruhella23@gmail.com>
2026-03-22 00:17:44 +0530

    search insertion

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -43,3 +43,29 @@ var searchInsert = function(nums, target) {
     return left;
 };
 
+//2nd approach binary search 
+
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    // position where target should be inserted
+    return left;
+};
+
+
+
