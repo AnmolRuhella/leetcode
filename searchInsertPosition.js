e57ccf5dc11a1956949aa007ea71cb1ce9410567
AnmolRuhella <anmolruhella23@gmail.com>
2026-03-22 00:19:09 +0530

    search insert position

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -15,7 +15,7 @@ var searchInsert = function(nums, target) {
     
 };
 
-//another approach binary search
+//2nd approach binary search
 
 const nums = [1,3,5,6]
 const target =  2
@@ -43,29 +43,7 @@ var searchInsert = function(nums, target) {
     return left;
 };
 
-//2nd approach binary search 
 
-var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] === target) {
-            return mid;
-        }
-        else if (nums[mid] < target) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
-    }
-
-    // position where target should be inserted
-    return left;
-};
 
 
 
