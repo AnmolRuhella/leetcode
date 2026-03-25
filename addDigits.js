9d72d01c41c2c7dfdaa96cf44d25091da5c0cb45
AnmolRuhella <anmolruhella23@gmail.com>
2026-03-26 01:11:27 +0530

    add digits

---
+++ addDigits.js
@@ -0,0 +1,24 @@
+//leetcode question 258 
+var addDigits = function(num) {
+    while (num >= 10) {
+        let sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+
+        num = sum;
+    }
+
+    return num;
+};
+
+//2nd approach digital route  
+var addDigits = function(num) {
+    
+if (num === 0) return 0;
+    return 1 + (num - 1) % 9;
+  
+   };
+
