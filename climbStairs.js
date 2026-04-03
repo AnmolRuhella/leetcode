a1894fd3326907b4ba4069c985a7d03ce854260b
AnmolRuhella <anmolruhella23@gmail.com>
2026-04-04 00:43:43 +0530

    climb stairs

---
+++ climbStairs.js
@@ -0,0 +1,9 @@
+// leet code question number - 70
+var climbStairs = function(n, memo = {}) {
+    if(n in memo) return memo[n];
+    if(n <= 1) return 1;
+
+    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+
+    return memo[n];
+};
