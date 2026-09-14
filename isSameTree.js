e85a5dfefe882e4c3b3119181e2ccf9e1756f6d3
AnmolRuhella <anmolruhella23@gmail.com>
2026-09-14 18:22:57 +0530

    is same tre question

---
+++ isSameTree.js
@@ -0,0 +1,19 @@
+//leet code question no - 100 
+
+var isSameTree = function(p,q){
+
+    if(p === null && q=== null){
+        return true;
+    }
+    if(p === null || q === null ){
+        return false;
+    }
+    if(p.val !== q.val){
+        return false
+    }
+
+    return isSameTree(p.left , q.left) && isSameTree(p.right , q.right)
+}
+
+
+//follow the recusrion technique here
