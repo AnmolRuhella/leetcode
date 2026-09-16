78172efd541e136f30b9e83f3f85c32570bd3ea0
AnmolRuhella <anmolruhella23@gmail.com>
2026-09-17 01:24:01 +0530

    can win game thoda different question tha

---
+++ canWin.js
@@ -0,0 +1,13 @@
+//leet code - 292 
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+    if(n % 4 === 0){
+        return false 
+    }
+    return true 
+    
+};
