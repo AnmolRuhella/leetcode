6774d85edcfb395cf6097ef8fee979e37462f382
AnmolRuhella <anmolruhella23@gmail.com>
2026-09-24 01:19:53 +0530

    fix issue

--- perfectNumber.js
+++ perfectNumber.js
@@ -3,7 +3,7 @@ var checkPerfectNumber = function(nums){
     let sum = 0 
     let n =   1
 
-    while(sum<nums){
+    while(n<nums){
         if(nums%n === 0 ){
             sum = sum + n 
         }
@@ -11,3 +11,6 @@ var checkPerfectNumber = function(nums){
     }
     return num === sum 
 }
+
+//phele hum ky krre the ki hum check krre thi ki agr sum less than rahegi num se toh sum mei add krte raho 
+//iska catch yehh ha ki n se compare krenege ki jb tk voh nums se km rahega tb tk hum check krte rahnege 
